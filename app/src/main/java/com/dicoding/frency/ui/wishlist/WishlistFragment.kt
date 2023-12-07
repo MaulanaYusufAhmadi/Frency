9733c13b64c250e8f459a78d92b446ad708e4b6f
maulanayusufahmadi <maulanayusufahmd@gmail.com>
2023-12-07 07:48:16 +0700

    add login and register ui

--- app/src/main/java/com/dicoding/frency/ui/wishlist/WishlistFragment.kt
+++ app/src/main/java/com/dicoding/frency/ui/wishlist/WishlistFragment.kt
@@ -1,4 +1,4 @@
-package com.dicoding.frency.ui.dashboard
+package com.dicoding.frency.ui.wishlist
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.dicoding.frency.databinding.FragmentDashboardBinding
+import com.dicoding.frency.databinding.FragmentWishlistBinding
 
-class DashboardFragment : Fragment() {
+class WishlistFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentWishlistBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val wishlistViewModel =
+            ViewModelProvider(this).get(WishlistViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentWishlistBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        wishlistViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
